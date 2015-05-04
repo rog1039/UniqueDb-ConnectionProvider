4ef8fa40b0a39965f6b3c73dd97f5d456cfa8ee3
Paul Rogero <progero@gmail.com>
2015-05-03 22:34:26 -0400

    Added missing file.

---
+++ src/UniqueDb.ConnectionProvider/DataGeneration/SqlTableFactory.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Configuration;
+
+namespace UniqueDb.ConnectionProvider.DataGeneration
+{
+    public static class SqlTableFactory
+    {
+        public static SqlTable Create(SqlTableReference sqlTableReference)
+        {
+            var columns = InformationSchemaMetadataExplorer.GetInformationSchemaColumns(sqlTableReference);
+            var sqlColumns = columns.Select(SqlColumnFactory.Create).ToList();
+            var sqlTable = new SqlTable()
+            {
+                Name = sqlTableReference.TableName,
+                Schema = sqlTableReference.SchemaName,
+                SqlColumns = sqlColumns
+            };
+            return sqlTable;
+        }
+    }
+
+    public static class DataColumnGenerator
+    {
+        public static IList<DataColumn> RetrieveDataColumnsFromQuery(ISqlConnectionProvider sqlConnectionProvider, string sqlQuery)
+        {
+            var sqlDataReader = GetDataReaderFromQuery(sqlConnectionProvider, sqlQuery);
+            var columns = ExtractColumnsFromDataReader(sqlDataReader);
+            return columns;
+        }
+
+        private static SqlDataReader GetDataReaderFromQuery(ISqlConnectionProvider sqlConnectionProvider, string sqlQuery)
+        {
+            var connection = sqlConnectionProvider.GetSqlConnection();
+            connection.Open();
+            var query = new SqlCommand(sqlQuery, connection);
+            return query.ExecuteReader();
+        }
+
+        private static List<DataColumn> ExtractColumnsFromDataReader(SqlDataReader sqlDataReader)
+        {
+            var columns = new List<DataColumn>();
+            var schemaTable = sqlDataReader.GetSchemaTable();
+            foreach (var row in schemaTable.Rows)
+            {
+                var column = new ColumnInfo();
+            }
+            
+
+            
+            foreach (var column in schemaTable.Columns)
+            {
+                columns.Add((DataColumn) column);
+            }
+            return columns;
+        }
+    }
+
+    internal class ColumnInfo
+    {
+        public string ColumnName { get; set; }
+        public string ColumnOrdinal { get; set; }
+
+    }
+}
