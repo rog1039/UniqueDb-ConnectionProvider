b6de95b34d03acc6f6ec4c256ca5d27cc8bb20f0
Paul Rogero <progero@gmail.com>
2015-05-03 22:48:35 -0400

    Removed unnecessary class from code.

--- src/UniqueDb.ConnectionProvider/DataGeneration/DataColumnToCSharpPropertyGenerator.cs
+++ src/UniqueDb.ConnectionProvider/DataGeneration/DataColumnToCSharpPropertyGenerator.cs
@@ -38,41 +38,6 @@ namespace UniqueDb.ConnectionProvider.DataGeneration
             return column.DataType.Name;
         }
     }
-    public class SysTypes
-    {
-        public string ColumnName { get; set; }
-        public int ColumnOrdinal { get; set; }
-        public int ColumnSize { get; set; }
-        public short NumericPrecision { get; set; }
-        public short NumericScale { get; set; }
-        public bool IsUnique { get; set; }
-        public bool IsKey { get; set; }
-        public string BaseServerName { get; set; }
-        public string BaseCatalogName { get; set; }
-        public string BaseColumnName { get; set; }
-        public string BaseSchemaName { get; set; }
-        public string BaseTableName { get; set; }
-        public Type DataType { get; set; }
-        public bool AllowDBNull { get; set; }
-        public int ProviderType { get; set; }
-        public bool IsAliased { get; set; }
-        public bool IsExpression { get; set; }
-        public bool IsIdentity { get; set; }
-        public bool IsAutoIncrement { get; set; }
-        public bool IsRowVersion { get; set; }
-        public bool IsHidden { get; set; }
-        public bool IsLong { get; set; }
-        public bool IsReadOnly { get; set; }
-        public Type ProviderSpecificDataType { get; set; }
-        public string DataTypeName { get; set; }
-        public string XmlSchemaCollectionDatabase { get; set; }
-        public string XmlSchemaCollectionOwningSchema { get; set; }
-        public string XmlSchemaCollectionName { get; set; }
-        public string UdtAssemblyQualifiedName { get; set; }
-        public int NonVersionedProviderType { get; set; }
-        public bool IsColumnSet { get; set; }
-    }
-
 
     public static class SqlQueryToCSharpPropertyGenerator
     {
@@ -121,10 +86,6 @@ namespace UniqueDb.ConnectionProvider.DataGeneration
             if (typeName == "Int32")
             {
                 return "int";
-            }
-            if (typeName == "SqlHierarchyId")
-            {
-                return "SqlHierarchyId";
             }
             return typeName;
         }
