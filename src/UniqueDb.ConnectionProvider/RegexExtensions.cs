d071214992fe8914655c308955e52502be85c2df
Paul Rogero <progero@gmail.com>
2015-08-25 17:27:47 -0400

    Added dependencies to the nuspec file.

--- src/UniqueDb.ConnectionProvider/RegexExtensions.cs
+++ src/UniqueDb.ConnectionProvider/RegexExtensions.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Text;
 using System.Text.RegularExpressions;
 using UniqueDb.ConnectionProvider.DataGeneration;
-using YamlDotNet.Serialization;
 
 namespace UniqueDb.ConnectionProvider
 {
