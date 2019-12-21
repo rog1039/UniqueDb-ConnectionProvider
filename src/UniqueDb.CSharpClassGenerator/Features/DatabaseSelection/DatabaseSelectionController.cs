ed3a05d14f2252f19ec574924c5a1cb8c643da14
paulrogero <progero@gmail.com>
2019-12-21 08:46:05 -0500

    Switched over to Microsoft.Data.SqlClient.

--- src/UniqueDb.CSharpClassGenerator/Features/DatabaseSelection/DatabaseSelectionController.cs
+++ src/UniqueDb.CSharpClassGenerator/Features/DatabaseSelection/DatabaseSelectionController.cs
@@ -2,9 +2,9 @@
 using System.Data;
 using System.Data.Common;
 using System.Data.Odbc;
-using System.Data.SqlClient;
 using System.Linq;
 using Dapper;
+using Microsoft.Data.SqlClient;
 using Newtonsoft.Json;
 
 namespace UniqueDb.CSharpClassGenerator.Features.DatabaseSelection
