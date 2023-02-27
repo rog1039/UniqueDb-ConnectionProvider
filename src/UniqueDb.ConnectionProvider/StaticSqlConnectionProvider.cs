d41161f1f16ccc187596faa24e4c7f27bbeb2d92
Paul Rogero <progero@gmail.com>
2023-02-27 09:38:16 -0500

    Updated default logging action to Console.WriteLine and also changed StaticSqlConnectionProvider.ToString() to be easier to read

--- src/UniqueDb.ConnectionProvider/StaticSqlConnectionProvider.cs
+++ src/UniqueDb.ConnectionProvider/StaticSqlConnectionProvider.cs
@@ -4,48 +4,51 @@ namespace UniqueDb.ConnectionProvider;
 
 public class StaticSqlConnectionProvider : BaseSqlConnectionProvider
 {
-    public StaticSqlConnectionProvider(string serverName, string databaseName)
-    {
-        ServerName                  = serverName;
-        DatabaseName                = databaseName;
-        UseIntegratedAuthentication = true;
-    }
+   public StaticSqlConnectionProvider(string serverName, string databaseName)
+   {
+      ServerName                  = serverName;
+      DatabaseName                = databaseName;
+      UseIntegratedAuthentication = true;
+   }
 
-    public StaticSqlConnectionProvider(string serverName, string databaseName, string userName, string password)
-        : this(serverName, databaseName)
-    {
-        UserName                    = userName;
-        Password                    = password;
-        UseIntegratedAuthentication = false;
-    }
+   public StaticSqlConnectionProvider(string serverName, string databaseName, string userName, string password)
+      : this(serverName, databaseName)
+   {
+      UserName                    = userName;
+      Password                    = password;
+      UseIntegratedAuthentication = false;
+   }
 
-    protected StaticSqlConnectionProvider(){}
+   protected StaticSqlConnectionProvider() { }
 
-    public StaticSqlConnectionProvider(string connectionString)
-    {
-        var connectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
+   public StaticSqlConnectionProvider(string connectionString)
+   {
+      var connectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
 
-        ServerName   = connectionStringBuilder.DataSource;
-        DatabaseName = connectionStringBuilder.InitialCatalog;
+      ServerName   = connectionStringBuilder.DataSource;
+      DatabaseName = connectionStringBuilder.InitialCatalog;
 
-        var hasPassword = !string.IsNullOrWhiteSpace(connectionStringBuilder.Password);
-        var hasUser     = !string.IsNullOrWhiteSpace(connectionStringBuilder.UserID);
-        if (hasUser && hasPassword)
-        {
-            UserName                    = connectionStringBuilder.UserID;
-            Password                    = connectionStringBuilder.Password;
-            UseIntegratedAuthentication = false;
-        }
-        else
-        {
-            UseIntegratedAuthentication = true;
-        }
-    }
+      var hasPassword = !string.IsNullOrWhiteSpace(connectionStringBuilder.Password);
+      var hasUser     = !string.IsNullOrWhiteSpace(connectionStringBuilder.UserID);
+      if (hasUser && hasPassword)
+      {
+         UserName                    = connectionStringBuilder.UserID;
+         Password                    = connectionStringBuilder.Password;
+         UseIntegratedAuthentication = false;
+      }
+      else
+      {
+         UseIntegratedAuthentication = true;
+      }
+   }
 
-    public static ISqlConnectionProvider Blank => new StaticSqlConnectionProvider("", "");
+   public static ISqlConnectionProvider Blank => new StaticSqlConnectionProvider("", "");
 
-    public override string ToString()
-    {
-        return $"{ServerName}\\{DatabaseName} | IntegAuth: {UseIntegratedAuthentication} | User: {UserName}";
-    }
+   public override string ToString()
+   {
+      if (UseIntegratedAuthentication)
+         return $"{ServerName}\\{DatabaseName} | IntegAuth: Yes";
+      
+      return $"{ServerName}\\{DatabaseName} | NoIntegAuth; User: {UserName}";
+   }
 }
